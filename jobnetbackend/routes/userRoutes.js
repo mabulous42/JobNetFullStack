c0db7a0dfd6421eb977d1db397dfcbcc2f264d61
Abbas <tundeeey@gmail.com>
2023-07-20 17:55:18 +0100

    added validation to user's input like the username must not contain special character, password must contain an upper case, number and a special character

--- jobnetbackend/routes/userRoutes.js
+++ jobnetbackend/routes/userRoutes.js
@@ -1,7 +1,10 @@
 const userRoutes = require("express").Router()
 const { registerAsEmployer, registerAsUser } = require("../controllers/userControllers")
+const { userValidationSchema, employerValidationSchema } = require("../middlewares/userValidationSchema")
+const { validate } = require("../middlewares/validator")
 
-userRoutes.post("/registerAsEmployer", registerAsEmployer)
-userRoutes.post("/registerAsUser", registerAsUser)
+
+userRoutes.post("/registerAsEmployer", validate(employerValidationSchema), registerAsEmployer)
+userRoutes.post("/registerAsUser", validate(userValidationSchema), registerAsUser)
 
 module.exports = userRoutes
