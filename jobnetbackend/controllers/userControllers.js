de2634dd62cf254d1257aa571bc9b1770e33a10f
Abbas <tundeeey@gmail.com>
2023-07-22 18:00:47 +0100

    added employer and user login controller to the backend and also created a job seeker (user) sign up page in the frontend

--- jobnetbackend/controllers/userControllers.js
+++ jobnetbackend/controllers/userControllers.js
@@ -1,4 +1,6 @@
 const { employerModel, userModel } = require("../models/user.models")
+const bcryptjs = require("bcryptjs")
+const { generateToken, verifyToken } = require("../services/sessions")
 const { sendMessage } = require("../utilities/mailer")
 
 const registerAsEmployer = async(req,res,next) => {
@@ -37,4 +39,46 @@ const registerAsUser = async(req,res,next) => {
     }
 }
 
-module.exports = {registerAsEmployer, registerAsUser}
+const userLogin = async (req, res, next) => {
+    try {
+        const { email, password } = req.body
+        console.log(email, password)
+        const user = await userModel.findOne({ email })
+        console.log(user)
+        if (!user) {
+            return res.status(404).send({ message: "You do not have an account with us", status:false })
+        }
+        const isMatch = await bcryptjs.compare(password, user.password)
+        console.log(isMatch);
+        if (!isMatch) {
+            return res.status(401).send({ message: "Invalid password", status: false })
+        }
+        const token = generateToken(email);
+        return res.status(200).send({ message: `Welcome ${user.userName}`, status: true, token })
+    } catch (error) {
+        next(error)
+    }
+}
+
+const employerLogin = async (req, res, next) => {
+    try {
+        const { email, password } = req.body
+        console.log(email, password)
+        const user = await employerModel.findOne({ email })
+        console.log(user)
+        if (!user) {
+            return res.status(404).send({ message: "You do not have an account with us", status:false })
+        }
+        const isMatch = await bcryptjs.compare(password, user.password)
+        console.log(isMatch);
+        if (!isMatch) {
+            return res.status(401).send({ message: "Invalid password", status: false })
+        }
+        const token = generateToken(email);
+        return res.status(200).send({ message: `Welcome ${user.userName}`, status: true, token })
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports = {registerAsEmployer, registerAsUser, userLogin, employerLogin}
--- jobnetbackend/routes/userRoutes.js
+++ jobnetbackend/routes/userRoutes.js
@@ -1,10 +1,12 @@
 const userRoutes = require("express").Router()
-const { registerAsEmployer, registerAsUser } = require("../controllers/userControllers")
+const { registerAsEmployer, registerAsUser, userLogin, employerLogin } = require("../controllers/userControllers")
 const { userValidationSchema, employerValidationSchema } = require("../middlewares/userValidationSchema")
 const { validate } = require("../middlewares/validator")
 
 
 userRoutes.post("/registerAsEmployer", validate(employerValidationSchema), registerAsEmployer)
 userRoutes.post("/registerAsUser", validate(userValidationSchema), registerAsUser)
+userRoutes.get("/userLogin", userLogin)
+userRoutes.get("/employerLogin", employerLogin)
 
 module.exports = userRoutes
