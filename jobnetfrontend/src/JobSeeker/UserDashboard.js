f43204450672746436d5f238b8d6bc99691cb78f
Abbas <tundeeey@gmail.com>
2023-08-17 18:43:09 +0100

    added styling to the displayed jobs

--- jobnetfrontend/src/JobSeeker/UserDashboard.js
+++ jobnetfrontend/src/JobSeeker/UserDashboard.js
@@ -28,7 +28,7 @@ function UserDashboard() {
         })
     }, [])
 
-    
+
     //fetching all the posted jobs from the database
     useEffect(() => {
         const uri = "http://localhost:5353/users/allJobs"
@@ -37,11 +37,11 @@ function UserDashboard() {
             let postJobs = res.data
             console.log(postJobs);
             console.log(userSkills);
-            const filteredJobs  = postJobs.filter(job =>
+            const filteredJobs = postJobs.filter(job =>
                 userSkills.some(skill => job.requiredSkills.includes(skill))
             );
             setallJobs(filteredJobs)
-            
+
         }).catch((err) => {
             console.log(err)
         })
@@ -68,52 +68,58 @@ function UserDashboard() {
                 <button onClick={showJobs} className="btn btn-warning">Jobs</button>
                 {
                     allJobs.map((jobs, index) => (
-                        <div className='px-3 jobs-div w-75' key={index}>
+                        <div className='px-3 jobs-div w-75 job-display-div' key={index}>
                             <div className='bg-white shadow px-4 py-4 rounded mb-1 w-100' >
                                 <div className='d-flex mb-2'>
-                                    <h5 className='me-3'>{jobs.jobDescription}</h5>
+                                    <h5 className='me-3 job-desc-text'>{jobs.jobDescription}</h5>
                                     <div className=''>
-                                        <div className='d-flex job-type text-primary px-2'>
+                                        <div className='d-flex align-items-center justify-content-center job-type text-primary px-2 rounded'>
                                             <div className='me-1'>
                                                 <i className="bi bi-briefcase"></i>
                                             </div>
-                                            <div>{jobs.jobType}</div>
+                                            <div className='jobType-text'>{jobs.jobType}</div>
                                         </div>
                                     </div>
                                 </div>
                                 <div className='d-flex mb-2'>
-                                    <div className='d-flex shadow bg-white px-2 me-3 rounded'>
-                                        <div className='me-1'>
-                                            <i className="bi bi-bag-check"></i>
+                                    <div>
+                                        <div className='d-flex shadow bg-white px-2 me-3 rounded'>
+                                            <div className='me-1'>
+                                                <i className="bi bi-bag-check"></i>
+                                            </div>
+                                            <div className='jobTitle-text'>{jobs.jobTitle}</div>
                                         </div>
-                                        <div>{jobs.jobTitle}</div>
                                     </div>
-                                    <div className='d-flex shadow bg-white px-2 me-3 rounded-sm'>
-                                        <div className='me-1'>
-                                            <i className="bi bi-calendar2"></i>
+                                    <div>
+                                        <div className='d-flex shadow bg-white px-2 me-3 rounded-sm'>
+                                            <div className='me-1'>
+                                                <i className="bi bi-calendar2"></i>
+                                            </div>
+                                            <div>{jobs.date}</div>
                                         </div>
-                                        <div>{jobs.date}</div>
                                     </div>
-                                    <div className='d-flex shadow bg-white px-2 me-3 rounded'>
-                                        <div className='me-1'>
-                                            <i className="bi bi-wallet"></i>
+                                    <div>
+                                        <div className='d-flex shadow bg-white px-2 me-3 rounded'>
+                                            <div className='me-1'>
+                                                <i className="bi bi-wallet"></i>
+                                            </div>
+                                            <div> ₦{jobs.min_salary} - ₦{jobs.max_salary} ({jobs.salaryType})</div>
                                         </div>
-                                        <div> ₦{jobs.min_salary} - ₦{jobs.max_salary} ({jobs.salaryType})</div>
                                     </div>
                                 </div>
-                                <div className='d-flex align-items-center justify-content-between'>
-                                    <div className='d-flex'>
-                                        <div className='me-1'>Skills: </div>
+                                <div className='mt-3'>
+                                    <h6 className='me-1'>Skills: </h6>
+                                    <div className='d-flex flex-wrap'>
                                         {
                                             jobs.requiredSkills.map((skill, i) => (
-                                                <div className='me-1 bg-warning' key={i}>
-                                                    <div className='d-flex'>{skill}</div>
+                                                <div className='me-1 mb-1 skill-div rounded px-2' key={i}>
+                                                    <div className='d-flex skill-text'>{skill}</div>
                                                 </div>
                                             ))
                                         }
                                     </div>
                                     <div className='mt-3 text-end'>
-                                        <button className="btn btn-success">Apply for Job</button>
+                                        <button className="btn btn-dark">Apply for Job</button>
                                     </div>
                                 </div>
                             </div>
